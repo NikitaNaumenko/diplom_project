5b51befcb18999b3981664b650eb0d7a1397dbd9
Naumenko Nikita <naumenkoniki@gmail.com>
2018-02-11 18:21:49 +0300

    first commit

---
+++ make-app.mk
@@ -0,0 +1,14 @@
+USER = "$(shell id -u):$(shell id -g)"
+
+app:
+	docker-compose up
+
+app-build:
+	docker-compose build
+app-setup: app-build
+	docker-compose run --user=$(USER) app bin/setup
+
+# app-bash:
+# 	docker-compose run --user=$(USER) app bash
+app-bash:
+	docker-compose run app bash
