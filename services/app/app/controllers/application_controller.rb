d094a8eaf178d383afabdbd4cfcfbb0f621cab67
Naumenko Nikita <naumenkoniki@gmail.com>
2019-02-17 19:20:27 +0300

    add responders

--- services/app/app/controllers/application_controller.rb
+++ services/app/app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
+require "application_responder"
+
 # frozen_string_literal: true
 
 # Main controller
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
   protect_from_forgery with: :exception
 end
